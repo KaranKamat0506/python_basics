ff8ea3ff3821964a6e7e457649a1575179bf19f6
KARAN <karankamat2000@gmail.com>
2019-09-02 19:02:25 +0530

    third commit

---
+++ ifelif.py
@@ -0,0 +1,7 @@
+marks=int(input("Enter the marks:"))
+if marks>=90:
+    print("Congratulations! You have scored A grade")
+elif marks>=40:
+    print("Congratulations! You have cleared the exam")
+else:
+    print("You have failed the exam")
