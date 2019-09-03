1313e3f43e9bc8f0ca62dba3de2eddab086ecc7b
KARAN <karankamat2000@gmail.com>
2019-09-03 22:10:08 +0530

    Tupple

---
+++ nestedloops.py
@@ -0,0 +1,7 @@
+#matrix
+no=1
+for row in range(1,4):
+    for col in range(1,4):
+        print(no,end=" ")
+        no+=1
+    print()
---
+++ tuple.py
@@ -0,0 +1,3 @@
+historicWarDates=[1914,1939]
+print("World war 1 took place in the year {}".format(historicWarDates[0]))
+print(historicWarDates[1])
