37c04b1c805d2b3ae780c632db0597d609dd9e8c
KARAN <karankamat2000@gmail.com>
2019-09-04 19:55:40 +0530

    Modifications made in tuple.py

--- tuple.py
+++ tuple.py
@@ -1,3 +1,8 @@
-historicWarDates=[1914,1939]
+historicWarDates=(1914,1939)
 print("World war 1 took place in the year {}".format(historicWarDates[0]))
 print(historicWarDates[1])
+# historicWarDates[1]=2017 error try this once
+print(historicWarDates[1])
+# Delete historic war dates
+del(historicWarDates)
+print(historicWarDates)
