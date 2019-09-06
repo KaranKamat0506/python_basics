17e6e59f01688529cf2d64ad1773137b3d7b9a50
KARAN <karankamat2000@gmail.com>
2019-09-06 19:12:05 +0530

    String

---
+++ string.py
@@ -0,0 +1,24 @@
+fruit="Apple"
+print(fruit)
+print(fruit[0])
+print(fruit[4])
+print(fruit[-1])
+print(fruit[-2])
+#string formatting
+print("Python is great programming language for beginners")
+programmingLang="Python"
+print("{} is great programming language for beginners".format(programmingLang))
+
+#operations performed on strings
+#upper()
+string="Python is powerful programming language"
+print(string.upper())
+#lower()
+print(string.lower())
+#replace()
+print(string.replace("powerful","great"))
+print(string)
+#slice()
+print(string[0:6])
+#length()
+print(len(string))
