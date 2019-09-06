612d4f54344d53778ca9a91a9fe9898cefb2cbf1
KARAN <karankamat2000@gmail.com>
2019-09-06 18:18:47 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,22 @@
+#Operators used in python
+#Assigment operator
+x=5
+print(x)
+#Arithmetic operator
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(5//5)
+print(5**5)
+#Relational operator
+print(5>2)
+print(5<2)
+print(5>=2)
+print(5<=2)
+print(5!=2)
+print(5==2)
+#Logical Operator
+print(5>2 and 5>1)
+print(5<2 or 5>3)
+print(not(5<2))
