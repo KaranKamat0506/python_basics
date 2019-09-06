66d1e5663facfc6135763d230575edec0c0aefa5
KARAN <karankamat2000@gmail.com>
2019-09-06 17:50:37 +0530

    Numbers

---
+++ number.py
@@ -0,0 +1,10 @@
+integerNumber=2
+floatingNumber=2.75
+print(integerNumber)
+print(floatingNumber)
+print(type(integerNumber))
+print(type(floatingNumber))
+#typecasting to int
+floatingNumber=int(floatingNumber)
+print(floatingNumber)
+print(type(floatingNumber))
