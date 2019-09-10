f3c4714c94fcf5cb8712205c28de059f9837ec94
KARAN <karankamat2000@gmail.com>
2019-09-10 18:27:48 +0530

    Program implemented using self keyword

--- OOP/emp.py
+++ OOP/emp.py
@@ -1,14 +1,26 @@
 class Employee:
-    print("\nEnter the name of Employee:")
-    emp_name=input()
-    print("\nEnter the Salary of Employee:")
-    emp_sal=int(input())
-    print("\nName of the Employee: {}".format(emp_name))
-    print("\nSalary of the Employee: {}".format(emp_sal))
+    def __init__(self):   
+        #without self keyword
+        print("\nEnter the name of Employee:")
+        emp_name=input()
+        print("\nEnter the Salary of Employee:")
+        emp_sal=int(input())
+        print("\nName of the Employee: {}".format(emp_name))
+        print("\nSalary of the Employee: {}".format(emp_sal))
 
 emp1=Employee()
 emp2=Employee()
-print("\n",emp1)
-print("\n",emp2)
+
+
+#using self keyword
+class employee:
+    def __init__(self,name,salary):
+        self.name=name
+        self.salary=salary
+
+e1=employee("John",240000)
+e2=employee("Pat",450000)
+print("\n",e1.name,"\n",e1.salary)
+print("\n",e2.name,"\n",e2.salary)
 
 
