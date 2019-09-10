77bb93d65aba0f5c3652cc625993e325e73fe190
KARAN <karankamat2000@gmail.com>
2019-09-10 17:50:47 +0530

    Basic class program

---
+++ OOP/emp.py
@@ -0,0 +1,14 @@
+class Employee:
+    print("\nEnter the name of Employee:")
+    emp_name=input()
+    print("\nEnter the Salary of Employee:")
+    emp_sal=int(input())
+    print("\nName of the Employee: {}".format(emp_name))
+    print("\nSalary of the Employee: {}".format(emp_sal))
+
+emp1=Employee()
+emp2=Employee()
+print("\n",emp1)
+print("\n",emp2)
+
+
---
+++ OOP/user.py
@@ -0,0 +1,8 @@
+#program to define a class
+class User:
+    pass
+
+user1=User()
+user2=User()
+user3=User()
+print("\n",user1,"\n",user2,"\n",user3)
