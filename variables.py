6528c906118c6641280428bb88080814bc80743e
KaranKamat0506 <karankamat2000@gmail.com>
2020-08-11 20:40:13 +0530

    Variables

---
+++ variables.py
@@ -0,0 +1,16 @@
+x=100
+walter_white=1
+print(walter_white+x)
+
+# Variables can be reassigned
+python_is_awesome=100
+just_another_variable=python_is_awesome
+print(just_another_variable)
+python_is_awesome=200
+print(python_is_awesome)
+
+#Assigning Variables all at once
+all,at,once=1,2,3
+print(all,at,once)
+
+
