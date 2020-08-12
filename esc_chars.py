176c2b728d350237e88adaebc8861993a1977df1
KaranKamat0506 <karankamat2000@gmail.com>
2020-08-12 17:32:53 +0530

    String Escape Sequence

---
+++ esc_chars.py
@@ -0,0 +1,17 @@
+# For printing new line \n
+print("Hello\nWorld")
+new_line="Hey\nThere"
+print(new_line)
+
+#For printing Backslach \
+print("This is a backslash:\\")
+
+#For Backspace \b
+print("E\bHello")
+
+#For single and double qoutes put \ before the nested " or '
+print("Hello \"World\"")
+print('Hello \'World\'')
+
+mountains = "/\\/\\/\\"
+print(mountains)
