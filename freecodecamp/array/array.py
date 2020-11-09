010fa3a8c54bfb6558755ca6579d069f677895f7
KaranKamat0506 <karankamat2000@gmail.com>
2020-11-09 21:50:02 +0530

    arrays

---
+++ freecodecamp/array/array.py
@@ -0,0 +1,14 @@
+#Write a Python program to create an array of 5 integers and display the array items. Access individual element through indexes.
+
+array=[1,2,3,4,5]
+
+print("Printing the elements of the array")
+for element in array:
+    print(element)
+
+print("Accessing each element individually")
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
---
+++ freecodecamp/array/array_append.py
@@ -0,0 +1,7 @@
+#Write a Python program to append a new item to the end of the array.
+array_num=[1,2,3,4,5]
+
+str(array_num)
+print("Original String:" + str(array_num))
+array_num.append(6)
+print("New String:" + str(array_num))
