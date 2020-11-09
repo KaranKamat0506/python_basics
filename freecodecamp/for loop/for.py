bacff884ec0f64a2b64f8a792574275736c35e74
KaranKamat0506 <karankamat2000@gmail.com>
2020-11-09 19:22:58 +0530

    For loop

---
+++ freecodecamp/for loop/for.py
@@ -0,0 +1,3 @@
+array=["30","40","50"]
+for element in array:
+    print(element)
---
+++ freecodecamp/for loop/range_for.py
@@ -0,0 +1,12 @@
+#The below range function prints the nos from 0 to 9
+for element in range(10):
+    print(element)
+
+#The below range function prints the nos from 3 to 9
+for element in range(3,10):
+    print(element)
+
+office=["Jim","Pam","Micheal"]
+for employees in range(len(office)):
+    print(employees)
+
