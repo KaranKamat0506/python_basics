d94ee1b50876a0f8e8ff067d35284693c87db9c7
KaranKamat0506 <karankamat2000@gmail.com>
2020-11-11 19:21:03 +0530

    Exponential Function

---
+++ freecodecamp/exponent/exponent.py
@@ -0,0 +1,8 @@
+def exp(base_num,pow_num):
+    result=1
+    for index in range(pow_num):
+        result=result*base_num
+    print(result)
+base=int(input("Enter the Base Number: "))
+power=int(input("Enter the Power Number: "))
+exp(base,power)
