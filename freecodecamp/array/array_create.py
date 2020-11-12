7420798f7091d46356740326ebf9d4750c543b6f
KaranKamat0506 <karankamat2000@gmail.com>
2020-11-12 15:50:15 +0530

    Array Creation

---
+++ freecodecamp/array/array_create.py
@@ -0,0 +1,22 @@
+#Python program to demonstrate  
+# Creation of Array  
+  
+# importing "array" for array creations
+import array as arr
+
+#Creating an array with integer type
+a=arr.array('i',[1,2,3])
+
+#printing original integer type array
+for i in range(0,3):
+    print(a[i],end=" ")
+
+#creating an array with float type
+b=arr.array('f',[1.5,2.5,4.5])
+
+#printing original float type array
+for i in range(0,3):
+    print(b[i],end=" ")
+
+
+
