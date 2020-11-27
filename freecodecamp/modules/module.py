45479e9e8b7369d94f1fb5885ec13096a98468dd
KaranKamat0506 <karankamat2000@gmail.com>
2020-11-27 22:54:47 +0530

    Module

---
+++ freecodecamp/modules/module.py
@@ -0,0 +1,26 @@
+
+
+# def fib(n):
+#     a,b=0,1
+#     count=0
+#     #check if the number of terms is valid
+#     if n <=0:
+#         print("Please enter positive integer")
+#     elif n==1:
+#         print("Fibonacci sequence upto"+str(n)+":")
+#         print(a)
+#     else:
+#         print("Fibonacci Sequence:")
+#         while count < n:
+#             print(a)
+#             c=a+b
+#             a=b
+#             b=c
+#             count+=1
+            
+def add(a,b):
+    a=a+b
+    return a
+
+
+
