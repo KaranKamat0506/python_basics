514d1bbb15bc49a0b366e1ac266b7de3ec957667
KaranKamat0506 <karankamat2000@gmail.com>
2022-09-04 19:52:51 +0530

    Basics of python

---
+++ freecodecamp/variables/basics.py
@@ -0,0 +1,96 @@
+# a=1
+# b=2
+# str1="This is a basic file"
+# d=4.26
+# print(a+b)
+# # print(c)
+# print(type(d))
+
+# print(str1.find("basic"))
+
+# items=[1,2,3]
+# print(items)
+# items[1]="Name"
+# print(items)
+# print(type(items))
+
+# tup1=(1,2,3)
+# print(tup1)
+# print(type(tup1))
+
+# dict1={}
+# print(dict1)
+# dict1["Name"] = "Not Available"
+# print(dict1) 
+# print(dict1.get("Name"))
+# print(dict1.items())
+# print(dict1.keys())
+
+# list2=[1,1,2,3,4,5,3,2]
+# print(list2)
+# #but if you want to convert the list into set you have to typecast it
+# s1=set(list2)
+# print(s1)
+# s2={1,2,3,3,3,4}
+# print(s2)
+
+# a=3
+# b=4
+# c="Name"
+# print(str(a)+str(b)+c)
+# print("10 - 5 is ",10-5)
+# print("10 * 5 is ",10*5)
+# print("10 / 5 is ",int(10/5))
+# print("10 + 5 is ",10+5)
+
+# var = int(input())
+# print(var)
+# if var>4:
+#     print("Variable is greater")
+# elif var==2:
+#     print("Variable is two")
+# else:
+#     print("Variable is not greater")
+
+
+
+
+# for i in range(0,101):
+#     print(i)
+
+# i=0
+# while(i<101):
+#     print(i)
+#     i=i+1
+
+# def average(num1,num2):
+#     avr=(num1+num2)/2
+#     return avr
+
+# print(average(3,6))
+
+# index=3
+# try:
+#     print(index)
+# except Exception as e:
+#     print(e)
+
+# f=open("2.txt","w")
+# f.write("Hello World")
+# f.close()
+# f=open("2.txt","r")
+# content=f.read()
+# f.close()
+# print(content)
+
+
+# list3=[1,2,3,4,"Name","Name",4,4,67,6.89]
+# print(list3)
+# s3={1,2,3,4,"Name","Name",4,4,67,6.89}
+# print(s3)
+# tup3=(1,2,3,4,"Name","Name",4,4,67,6.89)
+# print(tup3)
+dict3={}
+dict3["Name"]="Name"
+print(dict3.get("Name"))
+
