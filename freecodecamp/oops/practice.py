2b0d306e110a1f8941724e4d8f6fadd153b598c4
KaranKamat0506 <karankamat2000@gmail.com>
2022-09-10 18:50:22 +0530

    Instance Variables and Class Variables

---
+++ freecodecamp/oops/practice.py
@@ -0,0 +1,18 @@
+class Employee:
+    pass
+
+# Object 
+walter=Employee()
+jesse=Employee()
+
+#Variables
+walter.fname="Walter"
+walter.lname="White"
+walter.age=52
+
+jesse.fname="Jesse"
+jesse.lname="Pinkman"
+jesse.age=30
+
+print(walter.fname)
+print(jesse.age)
